3570c7306b22e2e1ad33be7c3f74af1230e8fed9
shahid3222 <163285903+shahid3222@users.noreply.github.com>
2024-09-10 18:26:08 +0300

    Update z.py
    repair code

--- z.py
+++ z.py
@@ -10,9 +10,19 @@ class Triangle(Shape):
 class Rectangle(Shape):
     def area(self):
         return self.width * self.height
-
-triangle = Triangle(6, 10)
-rectangle = Rectangle(4, 10)
+g=1
+while g==1:
+    a=int(input("Введите ширину треугольника"))
+    b=int(input("Введите высоту треугольника"))
+    c=int(input("Введите ширину прямоугольника"))
+    d=int(input("Введите высоту прямоугольника"))
+    if a>0 and b>0 and c>0 and d>0:
+        g=2
+    else:
+        print("\n\n\n\n\nВводи только положительные числа")
+        g=1
+triangle = Triangle(a, b)
+rectangle = Rectangle(c, d)
 
 print(f"Площадь треугольника: {triangle.area()}")
 print(f"Площадь прямоугольника: {rectangle.area()}")
